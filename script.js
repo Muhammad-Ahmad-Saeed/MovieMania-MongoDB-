9dc1db817e4d472dd37e4752f170c2a6222e8f78
Muhammad Ahmad Saeed <ahmadsaeed3220@gmail.com>
2023-12-04 01:39:05 +0500

    Add CRUD Operations

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const {
+  create,
+  get,
+  delete: deleteMovie,
+  update,
+} = require("./movieOperation");
+
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1/MovieMania").then(() => {
+const startServer = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1/MovieMania");
+
     console.log("Connected to database");
-}).catch(err => {
+
+    //let createmovie = await create("The Prestige", 130, ["Christian Bale", "Hugh Jackman"]);
+    //console.log(createmovie);
+
+    //get all movies
+    //let getmovies = await get();
+    //console.log(getmovies);
+
+    //delete a movie
+    //await deleteMovie("656ce170ab8c76437034e663");
+
+    //update a movie
+    await update("656ce56be6ff9c836ca536db", "Insomnia", 120, ["Al Pacino", "Robin Williams"]);
+
+    const port = 3000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (err) {
     console.log("Could not connect ");
     console.log(err);
-})
+  }
+};
 
-app.listen(3000);
+startServer();
